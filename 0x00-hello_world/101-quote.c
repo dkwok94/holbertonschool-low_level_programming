3fe933658e4656d0cf77f43d68493cb0e39ab85e
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-01-18 00:42:51 +0000

    Update program to write to stderr
    Tried to use the write() function to write the string to the stderr stream.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-
+#include <errno.h>
+/**
+ *main - Prints a string to standard error
+ *
+ *Description: Prints the string specified to the stderr pointer without using
+ *the function fprintf, puts, printf, or any of its derivatives
+ *Return: Returns the value 1
+ */
 int main(void)
 {
-	char *string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	while(*string != '\0')
-	{
-		putchar(*string);
-		string++;
-	}
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
