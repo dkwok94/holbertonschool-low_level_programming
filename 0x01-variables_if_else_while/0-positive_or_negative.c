d24d563c54ebbfd192e35da46d0c67cedce5cdd8
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-01-18 18:43:33 +0000

    Add a program called 0-positive_or_negative.c
    This program will take a randomly generated integer number and print out whether it is negative, positive, or zero.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *@n: A number that will be generated at random
+ *
+ *Description: This program will take a random number and display whether it is
+ *positive, negative, or zero.
+ *Return: Returns 0 upon successful run.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
