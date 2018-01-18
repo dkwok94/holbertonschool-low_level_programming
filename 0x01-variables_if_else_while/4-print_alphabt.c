cfef058477478ab0e1f5787038f2a48ad0bc4fc5
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-01-18 21:17:31 +0000

    Recheck coding style to conform with Betty standards
    There was a line in my code that was over 80 characters in length.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,7 +2,8 @@
 /**
  *main - Entry point
  *
- *Description: This program will print all letters in lowercase except for q and e
+ *Description: This program will print all letters in lowercase except for
+ *q and e.
  *Return: 0 upon successful completion
  */
 int main(void)
