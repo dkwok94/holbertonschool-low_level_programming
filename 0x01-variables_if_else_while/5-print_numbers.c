ecd5e0706991734306f2d862278e49b8b8c24125
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-01-18 21:21:02 +0000

    Re-evaluate style guidelines
    There was a line in my function documentation that was over 80 characters which was a violation of my school's formatting guidelines.

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,8 @@
 /**
  *main - Entry point
  *
- *Description: The function prints all single digit numbers of base 10 starting from 0
+ *Description: The function prints all single digit numbers of base 10 starting
+ *from 0.
  *Return: 0 upon successful completion
  */
 int main(void)
