cd6a79eacb9d13e91921e12c0282460d9581f2ee
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-01-18 00:47:47 +0000

    Add header file for write command
    write() requires the header file unistd.h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <errno.h>
+#include <unistd.h>
 /**
  *main - Prints a string to standard error
  *
@@ -10,5 +10,5 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	return (1);
 }
