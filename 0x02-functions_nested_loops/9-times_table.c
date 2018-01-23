f0bf3b7ac10fcb6d8959a151ff8237ecd5c959c7
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-01-23 05:41:29 +0000

    Add a program called 9-times_table.c
    This program prints a multiplication table for 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ *times_table - prints a 9-times table
+ *
+ *Description: Prints a multiplication table of 9
+ *Return: 0 upon successful completion
+ */
+void times_table(void)
+{
+	int num = 0;
+	int row = 0;
+
+	while (row <= 9)
+	{
+		num = 0;
+		while (num <= 9)
+		{
+			if ((row * num) > 9)
+			{
+				_putchar(' ');
+				_putchar(((row * num) / 10) + '0');
+				_putchar(((row * num) % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((row * num) + '0');
+			}
+			if (num != 9)
+			{
+				_putchar(',');
+			}
+			num++;
+		}
+		_putchar('\n');
+		row++;
+	}
+}
