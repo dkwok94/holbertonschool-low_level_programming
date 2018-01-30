528e0dbd73efc7ba0586ba8b06d90c9a20e69fd5
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-01-30 00:22:33 +0000

    Add a function called 7-puts_half.c
    This function prints the upper half of a string

---
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ *puts_half - prints half of a string
+ *@str: string to print
+ *
+ *Return: void
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	char *origin = str;
+
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+
+	str = origin;
+	if ((length % 2) == 0)
+		str += (length / 2);
+	else
+		str += ((length - 1) / 2);
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
