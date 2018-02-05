86e8857ac63bcd8927711ed64158afaca352fbd4
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-05 20:54:22 +0000

    Add a function called 2-strchr.c
    Function locates a character in a string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_strchr - locates a character in a string
+ *@s: string to look into
+ *@c: character to look for
+ *
+ *Return: pointer to first occurance, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return NULL;
+}
