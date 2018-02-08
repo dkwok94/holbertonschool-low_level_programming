d97c03393dfd569bac8a0d4f1643986a4dc5a709
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-08 07:12:27 +0000

    Add a function called 100-wildcmp.c
    This function compares two strings for similarity

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+/**
+ *wildcmp - compares 2 strings for similarity
+ *@s1: first string
+ *@s2: second string
+ *
+ *Return: 1 if the two strings are the same, 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+	{
+		s1++;
+		s2++;
+		return (wildcmp(s1, s2));
+	}
+
+	else if (*s2 == '*')
+		return (asterisks(s1, s2));
+
+	else
+		return (0);
+}
+
+int asterisks(char *s1, char *s2)
+{
+
+	if (*s2 == '*')
+	{
+		s2++;
+		return (asterisks(s1, s2));
+	}
+
+	else if (*s2 == '\0')
+	{
+		if (*s2 != *s1)
+			return (0);
+		else
+			return (1);
+	}
+
+	else
+	{
+		if (*s1 != *s2)
+		{
+			s1++;
+			return (asterisks(s1, s2));
+		}
+
+		else if (*s1 == '\0')
+			return (0);
+
+		else
+		{
+			s1++;
+			s2++;
+			return (asterisks(s1, s2));
+		}
+	}
+}
