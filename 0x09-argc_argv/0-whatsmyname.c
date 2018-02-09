61ebf233e29fab908ff4e76f336124ca4a0bceb6
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-09 21:53:42 +0000

    Add a program called 0-whatsmyname.c
    This program prints its name followed by a new line

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argv: array of pointers to argument strings
+ *
+ *Description: Prints the program's name followed by newline
+ *Return: 0 when successfully run
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
