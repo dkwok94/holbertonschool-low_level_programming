90a50b90ecce85b51a57a3cd85b8dcfc9f015e57
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-10 00:16:48 +0000

    Add edge case for letters within numbers

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
  *main - entry point
  *@argc: number of command line arguments
@@ -11,7 +12,9 @@
 int main(int argc, char *argv[])
 {
 	int i = 1;
+	int j;
 	int sum = 0;
+	int isnum;
 
 	if (argc == 1)
 	{
@@ -21,15 +24,35 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (*argv[i] < '0' || *argv[i] > '9')
+		j = 0;
+		isnum = checknum(j, &(*argv[i]));
+		if (isnum == 1)
+			sum += atoi(argv[i]);
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		else
-			sum += atoi(argv[i]);
 		i++;
 	}
 	printf("%d\n", sum);
 	return (0);
+}
+
+/**
+ *checknum - checks if an argument is a number
+ *@j: counter for iteration over string
+ *@str: string to iterate over
+ *
+ *Return; 1 if number, 0 if not a number
+ */
+int checknum(int j, char *str)
+{
+	while (str[j] != '\0')
+	{
+		if (str[j] > '9' || str[j] < '0')
+			return (0);
+		j++;
+	}
+	return (1);
 }
