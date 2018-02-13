0f30970f6247fd9b8d9018989bf81a91a1b1b3db
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-13 21:23:43 +0000

    Remove NULL definition

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
  *create_array - creates an array of chars and initializes with specific char
@@ -13,12 +12,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-		return (NULL);
+		return (0);
 
 	char *chararray = malloc(sizeof(char) * size);
 
-	if (chararray == NULL)
-		return (NULL);
+	if (chararray == 0)
+		return (0);
 
 	while (i < size)
 	{
