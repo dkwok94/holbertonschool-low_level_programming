dce334e21f5a9089a7ea832c8b65577f8d29cc38
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-14 00:45:20 +0000

    Add free() statements if malloc fails in the middle

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -24,12 +24,15 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
 			return (NULL);
+		}
 
 		for (j = 0; j < width; j++)
-		{
 			grid[i][j] = 0;
-		}
 	}
 	return (grid);
 }
