a9b2223f6937f3cd7ca08f9d926069043f987c4f
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-15 22:15:11 +0000

    Typecast char array with 0 ASCII value

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	while (counter < nmemb)
 	{
-		*((unsigned int *)array + counter) = 0;
+		*((char *)array + counter) = 0;
 		counter++;
 	}
 	return ((void *)array);
