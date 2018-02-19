d9af99c70518c97e4848e33b71693a92ef25cddc
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-19 22:41:47 +0000

    Duplicate name and owner when passed in

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -12,13 +12,53 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *dupName;
+	char *dupOwner;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	dupName = _strdup(name);
+	dupOwner = _strdup(owner);
+	new_dog->name = dupName;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = dupOwner;
 
 	return (new_dog);
+}
+
+/**
+ *_strdup - duplicates a string
+ *@str: string to duplicate
+ *
+ *Return: pointer to duplicated string
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int length = 0;
+	char *string;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + i) != '\0')
+	{
+		length++;
+		i++;
+	}
+
+	i = 0;
+	string = malloc(sizeof(char) * (length + 1));
+
+	if (string == NULL)
+		return (NULL);
+
+	while (i < length + 1)
+	{
+		*(string + i) = *(str + i);
+		i++;
+	}
+	*(string + i) = '\0';
+	return (string);
 }
