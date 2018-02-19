e171bbf859ac8bb6c9e811074b7f77d56b0874c1
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-19 21:28:43 +0000

    Define typedef of struct after struct

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -8,13 +8,14 @@
  *
  *Description: defines a dog's name, age, and owner
  */
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
