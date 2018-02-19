131edd99880c416025d844e84b141d09a2d0f320
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-19 20:50:41 +0000

    Add main documentation for function

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ *main - entry point
+ *
+ *Description: prints the name of the file the executable is compiled with
+ *Return: 0 when successful
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
