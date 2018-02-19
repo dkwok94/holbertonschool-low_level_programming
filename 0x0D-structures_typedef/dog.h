61aba1ba91029987d07a10897d59acb01ff20789
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-19 21:16:04 +0000

    Add structure documentation for dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,8 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - data about a dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ *
+ *Description: defines a dog's name, age, and owner
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
 	char *owner;
 };
