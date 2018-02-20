a05e0e1a2fa1499e4406003a8a7340f6fa9808cd
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-20 00:12:11 +0000

    Add space before parenthesis on line 27

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -24,7 +24,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dupName == NULL)
 	{
 		free(new_dog);
-		return(NULL);
+		return (NULL);
 	}
 	dupOwner = _strdup(owner);
 	if (dupOwner == NULL)
