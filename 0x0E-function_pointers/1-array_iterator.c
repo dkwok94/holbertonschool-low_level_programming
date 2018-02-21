e86c58e3de24837be65b69c87bb77bf154452e51
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-21 21:26:19 +0000

    Add return values for NULL pointers

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -13,6 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
+	if (array == NULL || action == NULL)
+		return (-1);
+
 	while (i < size)
 	{
 		(*action)(*(array + i));
