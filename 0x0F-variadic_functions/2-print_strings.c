32dc9eaf6b3d84a63dc855ff90aadfc29a3b992c
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-02-24 20:18:48 +0000

    Change (nil) condition to if string == NULL

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	while (i < n)
 	{
 		string = va_arg(stringlist, char *);
-		if (*string == '\0')
+		if (string == NULL)
 			printf("(nil)");
 		else
 			printf("%s", string);
