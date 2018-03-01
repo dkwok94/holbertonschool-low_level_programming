1b329b1284ab43e918ab5d82949facb7a9cb2b4b
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-03-01 23:00:54 +0000

    Add a few free statements for failed cases

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -32,13 +32,19 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
-		free(head);
+		free(*head);
 		return (NULL);
 	}
 
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(*head);
+		return (NULL);
+	}
 	new->len = stringlen(str);
 	new->next = *head;
 	*head = new;
+	free(new);
 	return (*head);
 }
