05522a5339565a45c4facc8eedd76571f692556d
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-03-01 23:14:17 +0000

    Remove str check

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -29,14 +29,11 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
-	new = malloc(sizeof(list_t));
+       	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	if (new->str == NULL)
-		return (NULL);
-
 	new->len = stringlen(str);
 	new->next = *head;
 	*head = new;
