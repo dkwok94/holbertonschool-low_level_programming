72003e2d8569c63a3b53c4ac0ab0e5a57eadfe0e
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-03-02 05:49:38 +0000

    Add a function called 100-first.c
    This function prints a line of text before the main function is executed

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *printoverride - prints some text before main function is executed
+ *
+ *Return: void
+ */
+void printoverride(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
