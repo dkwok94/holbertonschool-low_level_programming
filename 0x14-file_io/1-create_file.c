52525cdccc362fcb4c4a737f20635ae9a65f76e3
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-03-13 22:14:43 +0000

    Put string counter into else bracket

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,12 +16,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (*(text_content + i) != '\0')
-	{
-		numletters++;
-		i++;
-	}
-
 	filedes = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (filedes == -1)
 		return (-1);
@@ -33,6 +27,12 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 	{
+		while (*(text_content + i) != '\0')
+		{
+			numletters++;
+			i++;
+		}
+
 		byteswritten = write(filedes, text_content, numletters);
 		if (byteswritten == -1)
 			return (-1);
