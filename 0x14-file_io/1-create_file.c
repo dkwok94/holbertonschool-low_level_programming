8b3720b4b6198384da3b17f912e3b4afc76fdb77
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-03-13 21:58:54 +0000

    Change order of open flags

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -22,14 +22,14 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	filedes = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	filedes = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (filedes == -1)
 		return (-1);
 
 	if (text_content == NULL)
 	{
 		close(filedes);
-		return(1);
+		return (1);
 	}
 	byteswritten = write(filedes, text_content, numletters);
 	if (byteswritten == -1)
