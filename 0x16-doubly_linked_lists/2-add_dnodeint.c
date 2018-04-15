82b9422d8e1cbb2f371d2a5fa01d2d7485bc45d1
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-04-15 21:19:53 +0000

    Add an edge case for the head double pointer

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -12,10 +12,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	if (*head == NULL)
 	{
