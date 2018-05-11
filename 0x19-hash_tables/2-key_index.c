ce912b4b78ade4dcd60337ce25ced73b501e8b8f
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-05-11 18:04:25 +0000

    Remove one line to make declarations on same line

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -9,8 +9,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash;
-	unsigned long int index;
+	unsigned long int hash, index;
 
 	hash = hash_djb2(key);
 	index = hash % size;
