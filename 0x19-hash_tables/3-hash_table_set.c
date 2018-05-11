03b00a9589b5712f078bd346be19afb08234b66d
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-05-11 18:12:23 +0000

    Reupload

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new;
+	hash_node_t *temp;
 
 	if (ht == NULL)
 		return (0);
@@ -18,7 +19,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (ht->size == 0 || ht->array == NULL)
 		return (0);
+
 	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			temp->value = (char *)value;
+			return (1);
+		}
+		temp = temp->next;
+	}
+
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
