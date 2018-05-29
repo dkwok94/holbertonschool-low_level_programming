03fd01fd11bd7cc5e138afc89e86936c72d1f670
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-05-29 21:31:52 +0000

    Add one more null check

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *cur, *t, *t2;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	for (cur = *list; cur != NULL; cur = cur->next)
