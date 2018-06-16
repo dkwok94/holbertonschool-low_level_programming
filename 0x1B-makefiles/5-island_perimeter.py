b9fef5ab7ff189c102526d1100ee9f5e073b1d13
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-06-16 02:21:39 +0000

    Add program that calculates perimeter of an island on a grid

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+"""
+
+def island_perimeter(grid):
+    """
+    """
+
+    patches = 0
+    borders = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                patches += 1
+                if i != len(grid) - 1:
+                    if grid[i + 1][j] == 1:
+                        borders += 1
+                if j != len(grid[i]) - 1:
+                    if grid[i][j + 1] == 1:
+                        borders += 1
+    perimeter = (patches * 4) - (borders * 2)
+    return perimeter
