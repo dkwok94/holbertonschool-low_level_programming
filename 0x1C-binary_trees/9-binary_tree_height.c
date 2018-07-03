b4d96f5fd7f0ba90eb256b864bf8ab251f238db1
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-07-03 21:33:41 +0000

    Fix return bug so that larger trees work - do not need helper function

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -1,23 +1,4 @@
 #include "binary_trees.h"
-/**
- *subheight - finds the height of a subtree
- *@tree: a pointer to the tree to count
- *@counter: a tally of the number of levels traversed
- *
- *Return: the height of the subtree
- */
-
-size_t subheight(const binary_tree_t *tree, size_t counter)
-{
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (counter);
-
-	else if (tree->left != NULL)
-		return (counter + subheight(tree->left, 1));
-
-	else
-		return (counter + subheight(tree->right, 1));
-}
 
 /**
  *binary_tree_height - measures the height of a binary tree
@@ -31,12 +12,15 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t leftheight;
 	size_t rightheight;
 
+	if (tree == NULL)
+		return (0);
+
 	/*Check if tree is just a root node*/
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	leftheight = subheight(tree->left, 1);
-	rightheight = subheight(tree->right, 1);
+	leftheight = binary_tree_height(tree->left) + 1;
+	rightheight = binary_tree_height(tree->right) + 1;
 
 	if (leftheight > rightheight)
 		return (leftheight);
