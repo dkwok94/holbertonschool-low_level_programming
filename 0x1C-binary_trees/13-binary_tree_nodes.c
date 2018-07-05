4ebe0581d24344cd60f0b8e279b610ef348bdbca
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-07-05 07:48:09 +0000

    Remove redundant code

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -1,23 +1,4 @@
 #include "binary_trees.h"
-/**
- *nodecounter - counts the number of nodes with at least one child in a subtree
- *@tree: the tree to count
- *
- *Return: the number of nodes in the tree with at least one child
- */
-size_t nodecounter(const binary_tree_t *tree)
-{
-	size_t count = 0;
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-
-	if (tree->left != NULL)
-		count += nodecounter(tree->left) + 1;
-	if (tree->right != NULL)
-		count += nodecounter(tree->right) + 1;
-	return (count);
-}
 
 /**
  *binary_tree_nodes - counts the nodes with at least 1 child
@@ -28,7 +9,7 @@ size_t nodecounter(const binary_tree_t *tree)
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t counter = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -37,8 +18,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left != NULL)
-		count += nodecounter(tree->left);
+		counter += binary_tree_nodes(tree->left);
 	if (tree->right != NULL)
-		count += nodecounter(tree->right);
-	return (count + 1);
+		counter += binary_tree_nodes(tree->right);
+
+	return (counter + 1);
 }
