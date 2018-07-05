1d7877133d43af104f0bd8324239980dbf127330
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-07-05 07:27:21 +0000

    Add one more edge case for a tree consisting only of root node

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *parent;
 
-	if (node == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
 	parent = node->parent;
