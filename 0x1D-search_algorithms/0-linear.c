6eb9f8a654f9c5762cc039739b961a29e0ee47e9
Derek <dhklangs@gmail.com>
2018-08-08 18:09:03 +0000

    Remove trailing whitespaces

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -10,7 +10,7 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
-	
+
 	if (array == NULL)
 		return (-1);
 
@@ -19,9 +19,9 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
-			
+
 		else
-			i++;	
+			i++;
 	}
 	return (-1);
 }
