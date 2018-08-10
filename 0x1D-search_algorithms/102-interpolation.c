17f40f8b588206ab382e6ed755f888f0f54fea8e
Derek <dhklangs@gmail.com>
2018-08-10 20:22:05 +0000

    Add edge case for array NULL check

--- 0x1D-search_algorithms/102-interpolation.c
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -13,6 +13,9 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t low = 0;
 	size_t pos;
 
+	if (array == NULL || size == 0)
+		return (-1)
+
 	while ((array[high] != array[low])
 	       && (value >= array[low])
 	       && (value <= array[high]))
