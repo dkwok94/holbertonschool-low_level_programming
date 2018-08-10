4a9d3a248514ae2f2d2a1ba0d1d96dce1216c7b2
Derek <dhklangs@gmail.com>
2018-08-10 20:22:52 +0000

    Add semicolon

--- 0x1D-search_algorithms/102-interpolation.c
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t pos;
 
 	if (array == NULL || size == 0)
-		return (-1)
+		return (-1);
 
 	while ((array[high] != array[low])
 	       && (value >= array[low])
